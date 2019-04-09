e3418e092326cc73a2e55879d01d7e5efe4247ad
chip reed <chip@chip.sh>
2019-04-08 20:44:35 -0700

    remove empty flag from byte trie

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,6 @@ where
     T: Debug,
 {
     root: Node<T>,
-    empty: bool,
 }
 
 impl<T> ByteTrie<T>
@@ -22,18 +21,11 @@ where
     pub fn new() -> Self {
         Self {
             root: Node::new_empty(),
-            empty: true,
         }
     }
 
     pub fn insert(&mut self, key: Key, value: T) {
-        if self.empty {
-            self.root.key = key;
-            self.root.value = Some(value);
-            self.empty = false;
-        } else {
-            self.root.insert(key, Some(value));
-        }
+        self.root.insert(key, Some(value));
     }
 }
 
